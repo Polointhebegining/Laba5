39d672c49e6bc7d78bf32b6dcf45a975a48bdecd
Полина Мелинти <108030042+Polointhebegining@users.noreply.github.com>
2024-01-09 23:11:10 +0300

    Update YourViewModel.cs

--- YourViewModel.cs
+++ YourViewModel.cs
@@ -1,8 +1,31 @@
+using System.Windows;
 public class YourViewModel : INotifyPropertyChanged
 {
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
     public ObservableCollection<TodoTask> TodoList { get; set; }
     public ICommand EditTaskCommand { get; private set; }
     public ICommand NewTaskCommand { get; private set; }
+
+    private TodoTask selectedTask;
+
+    public TodoTask SelectedTask
+    {
+        get { return selectedTask; }
+        set
+        {
+            if (selectedTask != value)
+            {
+                selectedTask = value;
+                OnPropertyChanged(nameof(SelectedTask));
+            }
+        }
+    }
 
     public YourViewModel()
     {
@@ -13,14 +36,31 @@ public class YourViewModel : INotifyPropertyChanged
 
     private void EditTask(object parameter)
     {
-        // Implementation for editing a task
+        if (SelectedTask != null)
+        {
+            var editWindow = new TaskDialogWindow();
+            editWindow.DataContext = SelectedTask.Clone(); // Тут нужно  клонирование для избежания изменений в оригинале
+            var result = editWindow.ShowDialog();
+
+            if (result == true)
+            {
+                // Тут обновляем TodoList и соответствующий объект TodoTask
+                int index = TodoList.IndexOf(SelectedTask);
+                TodoList[index] = (TodoTask)editWindow.DataContext;
+                OnPropertyChanged(nameof(TodoList));
+            }
+        }
     }
 
     private void CreateNewTask(object parameter)
     {
-        // Implementation for creating a new task
+        var newTaskWindow = new TaskDialogWindow();
+        var result = newTaskWindow.ShowDialog();
+
+        if (result == true)
+        {
+            TodoList.Add((TodoTask)newTaskWindow.DataContext);
+            OnPropertyChanged(nameof(TodoList));
+        }
     }
-
-    // INotifyPropertyChanged implementation...
 }
-
